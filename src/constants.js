172147de7aadc1f96efc5dc94117a801254e4746
vinodkv2511 <vinodkv2511@gmail.com>
2019-04-15 23:49:10 +0530

    Installed and setup redux-thunk

--- src/constants.js
+++ src/constants.js
@@ -21,7 +21,24 @@ const constants = {
 
 export const action_constants = {
     CHANGE_TAB : "home/CHANGE_TAB",
-    SEARCH : "home/SEARCH"
+    SEARCH : "home/SEARCH",
+    GET_POPULAR_MOVIES: "home/GET_POPULAR_MOVIES",
+    GET_TRENDING_MOVIES: "home/GET_TRENDING_MOVIES",
+    GET_POPULAR_MOVIES_SUCCESS: "home/GET_POPULAR_MOVIES_SUCCESS",
+    GET_POPULAR_MOVIES_FAILURE: "home/GET_POPULAR_MOVIES_FAILURE",
+    GET_TRENDING_MOVIES_SUCCESS: "home/GET_TRENDING_MOVIES_SUCCESS",
+    GET_TRENDING_MOVIES_FAILURE: "home/GET_TRENDING_MOVIES_FAILURE",
+}
+
+export const url_constants = {
+    API_BASE : "https://api.themoviedb.org/3",
+    API_POPULAR_PATH : "/movie/popular",
+    API_TRENDING : "/trending",
+    API_MOVIE_DETAILS : "/movie/",
+    TRENDING_MEDIA_TYPE_MOVIE_PATH: "/movie",
+    TRENDING_TIME_WINDOW_DAY: "day",
+    TRENDING_TIME_WINDOW_WEEK: "week",
+
 }
 
 export default constants;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose } from 'redux'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 import reducer from "./reducers"
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // For redux debugger
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // For redux debugger
     )
+)
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
-    </Provider>, 
+    </Provider>,
     document.getElementById('root')
 );
 
