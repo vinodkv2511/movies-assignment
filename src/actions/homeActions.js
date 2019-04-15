162c528b4796eb027db2eba4a0e340fe3ef241a6
vinodkv2511 <vinodkv2511@gmail.com>
2019-04-15 23:53:56 +0530

    Added action and reducer for home

--- src/actions/homeActions.js
+++ src/actions/homeActions.js
@@ -1,4 +1,5 @@
 import { action_constants } from '../constants'
+import { getPopularMovies as popularService, getTrendingMovies as trendingService } from '../services/homeService/homeService'
 
 
 const changeTab = (tabName)=>{
@@ -8,4 +9,54 @@ const changeTab = (tabName)=>{
     };
 }
 
-export { changeTab, }
+const getPopularMovies = (page) => {
+    return dispatch => {
+        popularService(page)
+        .then(res => res.json())
+        .then(data=> dispatch(getPopularMoviesSuccess(data)))
+        .catch(err => dispatch(getPopularMoviesFailure(err)))
+    }
+}
+
+
+const getPopularMoviesSuccess= (data)=> {
+    return {
+        type: action_constants.GET_POPULAR_MOVIES_SUCCESS,
+        payload: data
+    }
+}
+
+
+const getPopularMoviesFailure= (error)=> {
+    return {
+        type: action_constants.GET_POPULAR_MOVIES_FAILURE,
+        payload: error
+    }
+}
+
+const getTrendingMovies = (page, timeWindow) => {
+    return dispatch => {
+        trendingService(page,timeWindow)
+        .then(res => res.json())
+        .then(data=> dispatch(getTrendingMoviesSuccess(data)))
+        .catch(err => dispatch(getTrendingMoviesFailure(err)))
+    }
+}
+
+
+const getTrendingMoviesSuccess= (data)=> {
+    return {
+        type: action_constants.GET_TRENDING_MOVIES_SUCCESS,
+        payload: data
+    }
+}
+
+
+const getTrendingMoviesFailure= (error)=> {
+    return {
+        type: action_constants.GET_TRENDING_MOVIES_FAILURE,
+        payload: error
+    }
+}
+
+export { changeTab, getPopularMovies, getTrendingMovies }
--- src/reducers/homeReducer/homeReducer.js
+++ src/reducers/homeReducer/homeReducer.js
@@ -2,6 +2,10 @@ import consts, {action_constants} from '../../constants'
 
 const initialState = {
     activeTab: consts.tabs.POPULAR,
+    popularMovies: [],
+    popularMoviesCurrentPage: 1,
+    trendingMovies: [],
+    trendingMoviesCurrentPage: 1,
 }
 const homeReducer = (state= initialState, action) => {
     let newState = {}
@@ -9,6 +13,26 @@ const homeReducer = (state= initialState, action) => {
         case action_constants.CHANGE_TAB:
             newState = {...state}
             newState.activeTab = action.payload
+            break
+        case action_constants.GET_POPULAR_MOVIES_SUCCESS:
+            newState = {...state}
+            newState.popularMovies = [...action.payload.results]
+            newState.popularMoviesCurrentPage = action.payload.page
+            newState.popularMoviesPageCount = action.payload.total_pages
+            break
+        case action_constants.GET_POPULAR_MOVIES_FAILURE:
+            newState = {...state}
+            newState.popularMovies = []
+            break
+        case action_constants.GET_TRENDING_MOVIES_SUCCESS:
+            newState = {...state}
+            newState.trendingMovies = [...action.payload.results]
+            newState.trendingMoviesCurrentPage = action.payload.page
+            newState.trendingMoviesPageCount = action.payload.total_pages
+            break
+        case action_constants.GET_TRENDING_MOVIES_FAILURE:
+            newState = {...state}
+            newState.trendingMovies = []
             break
         case action_constants.SEARCH:
             newState = state
