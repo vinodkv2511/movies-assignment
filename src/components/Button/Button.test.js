f31c552bd9b5e26e4c076da865254adbabcd26c2
vinodkv2511 <vinodkv2511@gmail.com>
2019-04-19 10:52:25 +0530

    Added few more test cases to button

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -5,7 +5,9 @@ import Button from './Button'
 
 const dummyProps = {
     text: "Popular",
-    onClick: jest.fn()
+    onClick: jest.fn(),
+    isActive: true
+
 }
 
 describe('Rendering .. ', () => {
@@ -18,6 +20,22 @@ describe('Rendering .. ', () => {
     })
     it("Should render button text ", () => {
         expect(wrapper.text()).toEqual(dummyProps.text) 
+    })
+    it("should have class name tab_button and button_active", () => {
+        expect(wrapper.hasClass('tab_button')).toEqual(true)
+        expect(wrapper.hasClass('button_active')).toEqual(true)
+    })
+
+    it("should only have tab_button if isActive is not Passes", () => {
+        wrapper = shallow(<Button text="Pop" onClick={jest.fn()} />)
+        expect(wrapper.hasClass('tab_button')).toEqual(true)
+        expect(wrapper.hasClass('button_active')).toEqual(false)
+    })
+
+    it("should only have tab_button if isActive false", () => {
+        wrapper = shallow(<Button text="Pop" onClick={jest.fn()} isActive={false} />)
+        expect(wrapper.hasClass('tab_button')).toEqual(true)
+        expect(wrapper.hasClass('button_active')).toEqual(false)
     })
     
     
